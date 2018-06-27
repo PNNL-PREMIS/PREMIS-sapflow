652750a55f081be4c82155f72e65ad3f705a6f51
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-06-27 19:47:22 -0400

    Organize raw data

--- R/sapflow_dat.R
+++ R/sapflow_dat.R
@@ -6,14 +6,15 @@ library(lubridate)
 library(tibble)
 
 # Load data
-control <- read_csv("../sapflow_data/CR1000-CONTROL2_Table1.csv")
-salt <- read_csv("../sapflow_data/CR1000-SALT2_Table1.csv")
-fresh <- read_csv("../sapflow_data/CR1000-FRESH2_Table1.csv")
-shore <- read_csv("../sapflow_data/CR1000-SHORE2_Table1.csv")
+control <- read_csv("../sapflow_data/CR1000-CONTROL_Table1.csv")
+salt <- read_csv("../sapflow_data/CR1000-SALT_Table1.csv")
+fresh <- read_csv("../sapflow_data/CR1000-FRESH_Table1.csv")
+shore <- read_csv("../sapflow_data/CR1000-SHORE_Table1.csv")
 
 
 
-control_base <- tibble(Year = year(control$TIMESTAMP), Day = day(control$TIMESTAMP),
+control_base <- tibble(
+                       Year = year(control$TIMESTAMP), Day = day(control$TIMESTAMP),
                        Time = time(control$TIMESTAMP), VPD = NaN,
                        PAR = NaN, Sensor1 = control$`DiffVolt_Avg(1)`,
                        Sensor2 = control$`DiffVolt_Avg(2)`, Sensor3 = control$`DiffVolt_Avg(3)`,
