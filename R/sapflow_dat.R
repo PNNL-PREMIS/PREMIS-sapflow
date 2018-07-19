ce4208c32c6e44ff854cfed791e133691f2be23f
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-07-19 12:50:45 -0400

    Plot each site by tree

--- R/sapflow_dat.R
+++ R/sapflow_dat.R
@@ -4,22 +4,51 @@
 library(readr)
 library(lubridate)
 library(tibble)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 
-# Load data
-control <- read_csv("../sapflow_data/CR1000-CONTROL_Table1.csv")
-salt <- read_csv("../sapflow_data/CR1000-SALT_Table1.csv")
-fresh <- read_csv("../sapflow_data/CR1000-FRESH_Table1.csv")
-shore <- read_csv("../sapflow_data/CR1000-SHORE_Table1.csv")
+# Load data and prepare data to be combined into one dataframe
+cat("Loading data...")
+control <- read_csv("../sapflow_data/CR1000-CONTROL_Table1.csv", 
+                    col_names = c("Timestamp", "Record", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8")) %>%
+  melt(id.vars = c("Timestamp", "Record"), measure.vars = c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"))
+colnames(control) <-c("Timestamp", "Record", "Tree", "Voltage")
 
+salt <- read_csv("../sapflow_data/CR1000-SALT_Table1.csv", 
+                 col_names = c("Timestamp", "Record", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8")) %>% 
+  melt(id.vars = c("Timestamp", "Record"), measure.vars = c("S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"))
+colnames(salt) <-c("Timestamp", "Record", "Tree", "Voltage")
 
+fresh <- read_csv("../sapflow_data/CR1000-FRESH_Table1.csv", 
+                  col_names = c("Timestamp", "Record", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8")) %>% 
+  melt(id.vars = c("Timestamp", "Record"), measure.vars = c("F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"))
+colnames(fresh) <-c("Timestamp", "Record", "Tree", "Voltage")
 
-control_base <- tibble(
-                       Year = year(control$TIMESTAMP), Day = day(control$TIMESTAMP),
-                       Time = time(control$TIMESTAMP), VPD = NaN,
-                       PAR = NaN, Sensor1 = control$`DiffVolt_Avg(1)`,
-                       Sensor2 = control$`DiffVolt_Avg(2)`, Sensor3 = control$`DiffVolt_Avg(3)`,
-                       Sensor4 = control$`DiffVolt_Avg(4)`, Sensor5 = control$`DiffVolt_Avg(5)`,
-                       Sensor6 = control$`DiffVolt_Avg(6)`, Sensor7 = control$`DiffVolt_Avg(7)`,
-                       Sensor8 = control$`DiffVolt_Avg(8)`)
+shore <- read_csv("../sapflow_data/CR1000-SHORE_Table1.csv", 
+                  col_names = c("Timestamp", "Record", "Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6")) %>% 
+  melt(id.vars = c("Timestamp", "Record"), measure.vars = c("Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6"))
+colnames(shore) <-c("Timestamp", "Record", "Tree", "Voltage")
 
-write_csv(control_base, "../sapflow_data/control_base.csv", col_names = FALSE)
+# Combine into one dataframe
+cat("Combining into single dataframe...")
+combine <- bind_rows(control, fresh, salt, shore)
+
+# Plot using ggplot and facet by Tree
+cat("Generating plots...")
+control_plot <- ggplot(data = control, aes(x = Record, y = Voltage, group = Tree)) +
+  geom_line() +
+  facet_wrap(~Tree, ncol = 2)
+
+fresh_plot <- ggplot(data = fresh, aes(x = Record, y = Voltage, group = Tree)) +
+  geom_line() +
+  facet_wrap(~Tree, ncol = 2)
+
+salt_plot <- ggplot(data = salt, aes(x = Record, y = Voltage, group = Tree)) +
+  geom_line() +
+  facet_wrap(~Tree, ncol = 2)
+
+shore_plot <- ggplot(data = shore, aes(x = Record, y = Voltage, group = Tree)) +
+  geom_line() +
+  facet_wrap(~Tree, ncol = 2)
+
