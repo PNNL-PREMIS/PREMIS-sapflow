496d65628cf09c16ea2085d664b5eba29352d6f2
Stephanie Pennington <spenn1@terpmail.umd.edu>
2019-05-10 13:00:02 -0400

    Updated with new data

--- R/sapflow_dat.R
+++ R/sapflow_dat.R
@@ -11,21 +11,21 @@ library(reshape2)
 
 # Load data and prepare data to be combined into one dataframe
 cat("Loading data...")
-control <- read_csv("../sapflow_data/CR1000-CONTROL_Table1.csv", 
-                    col_names = c("Timestamp", "Record", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"))
-control$Timestamp <- mdy_hm(control$Timestamp)
+control <- read_csv("../SERC/sapflow_data/CR1000-CONTROL_Table1_20190510.dat", 
+                    col_names = c("Timestamp", "Record", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"), skip = 4)
+control$Timestamp <- ymd_hms(control$Timestamp)
 
-salt <- read_csv("../sapflow_data/CR1000-SALT_Table1.csv", 
-                 col_names = c("Timestamp", "Record", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8")) 
-salt$Timestamp <- mdy_hm(salt$Timestamp)
+salt <- read_csv("../SERC/sapflow_data/CR1000-SALT_Table1_20190510.dat", 
+                 col_names = c("Timestamp", "Record", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"), skip = 4) 
+salt$Timestamp <- ymd_hms(salt$Timestamp)
 
-fresh <- read_csv("../sapflow_data/CR1000-FRESH_Table1.csv", 
-                  col_names = c("Timestamp", "Record", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"))
-fresh$Timestamp <- mdy_hm(fresh$Timestamp)
+fresh <- read_csv("../SERC/sapflow_data/CR1000-FRESH_Table1_20190510.dat", 
+                  col_names = c("Timestamp", "Record", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"), skip = 4)
+fresh$Timestamp <- ymd_hms(fresh$Timestamp)
 
-shore <- read_csv("../sapflow_data/CR1000-SHORE_Table1.csv", 
-                  col_names = c("Timestamp", "Record", "Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6")) 
-shore$Timestamp <- mdy_hm(shore$Timestamp)
+shore <- read_csv("../SERC/sapflow_data/CR1000-SHORE_Table1_20190510.dat", 
+                  col_names = c("Timestamp", "Record", "Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6"), skip = 4) 
+shore$Timestamp <- ymd_hms(shore$Timestamp)
 
 load("../SERC_met_data_for_Charlotte_&_Stephanie.Rdata")
 
@@ -132,13 +132,13 @@ cat("Generating plots...")
 control_p <- melt(control, id.vars = c("Timestamp", "Record"), measure.vars = c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"))
 colnames(control_p) <-c("Timestamp", "Record", "Tree", "Voltage")
 
-salt_p <- melt(id.vars = c("Timestamp", "Record"), measure.vars = c("S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"))
+salt_p <- melt(salt, id.vars = c("Timestamp", "Record"), measure.vars = c("S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"))
 colnames(salt_p) <-c("Timestamp", "Record", "Tree", "Voltage")
 
-fresh_p <- melt(id.vars = c("Timestamp", "Record"), measure.vars = c("F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"))
+fresh_p <- melt(fresh, id.vars = c("Timestamp", "Record"), measure.vars = c("F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"))
 colnames(fresh_p) <-c("Timestamp", "Record", "Tree", "Voltage")
 
-shore_p <- melt(id.vars = c("Timestamp", "Record"), measure.vars = c("Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6"))
+shore_p <- melt(shore, id.vars = c("Timestamp", "Record"), measure.vars = c("Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6"))
 colnames(shore_p) <-c("Timestamp", "Record", "Tree", "Voltage")
 
 # ----- Quick plot for BC data ----- #
@@ -151,7 +151,7 @@ west <- read_csv("../BC/CR1000-WEST_2019_4_18_Table1.dat", skip = 4,
                  col_names = c("Timestamp", "Record", "W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8")) %>% 
   melt(id.vars = c("Timestamp", "Record"), measure.vars = c("W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8"))
 
-control_plot <- ggplot(data = control_p, aes(x = Timestamp, y = Voltage, group = Tree)) +
+ggplot(data = control_p, aes(x = Timestamp, y = Voltage, group = Tree)) +
   geom_line() +
   facet_wrap(~Tree, ncol = 2) +
   ylim(0,1) +
@@ -159,21 +159,21 @@ control_plot <- ggplot(data = control_p, aes(x = Timestamp, y = Voltage, group =
 print(control_plot)
 ggsave("../control.png")
 
-fresh_plot <- ggplot(data = fresh, aes(x = Timestamp, y = Voltage, group = Tree)) +
+ggplot(data = fresh_p, aes(x = Timestamp, y = Voltage, group = Tree)) +
   geom_line() +
   facet_wrap(~Tree, ncol = 2) +
   ylim(0,1) +
   ggtitle("Fresh")
 ggsave("../fresh.png")
 
-salt_plot <- ggplot(data = salt, aes(x = Timestamp, y = Voltage, group = Tree)) +
+ggplot(data = salt_p, aes(x = Timestamp, y = Voltage, group = Tree)) +
   geom_line() +
   facet_wrap(~Tree, ncol = 2) +
   ylim(0,1) +
   ggtitle("Salt")
 ggsave("../salt.png")
 
-shore_plot <- ggplot(data = shore, aes(x = Timestamp, y = Voltage, group = Tree)) +
+ggplot(data = shore_p, aes(x = Timestamp, y = Voltage, group = Tree)) +
   geom_line() +
   facet_wrap(~Tree, ncol = 2) + 
   ylim(0,1) +
