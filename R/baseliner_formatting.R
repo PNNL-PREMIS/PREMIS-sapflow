e210e3ffd4a0f64f66a6bfcc50fbea5e2231144c
Stephanie Pennington <spenn1@terpmail.umd.edu>
2019-09-16 17:06:55 -0400

    Updated code for simplicity

--- R/baseliner_formatting.R
+++ R/baseliner_formatting.R
@@ -12,45 +12,58 @@ library(dplyr)
 cat("Loading data...")
 
 # Sapflow data
-control <- read_csv("../SERC/sapflow_data/CR1000-CONTROL_Table1_20190510.dat", 
+control <- read_csv("../SERC/sapflow_data/raw_data/CR1000-CONTROL_Table1_20190829.dat", 
                     col_names = c("Timestamp", "Record", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"), skip = 4)
   control$Timestamp <- ymd_hms(control$Timestamp)
 
-salt <- read_csv("../SERC/sapflow_data/CR1000-SALT_Table1_20190510.dat", 
+salt <- read_csv("../SERC/sapflow_data/raw_data/CR1000-SEAWATER_Table1_20190829.dat", 
                  col_names = c("Timestamp", "Record", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"), skip = 4) 
   salt$Timestamp <- ymd_hms(salt$Timestamp)
 
-fresh <- read_csv("../SERC/sapflow_data/CR1000-FRESH_Table1_20190510.dat", 
+fresh <- read_csv("../SERC/sapflow_data/raw_data/CR1000-FRESH_Table1_20190829.dat", 
                   col_names = c("Timestamp", "Record", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"), skip = 4)
   fresh$Timestamp <- ymd_hms(fresh$Timestamp)
 
-shore <- read_csv("../SERC/sapflow_data/CR1000-SHORE_Table1_20190510.dat", 
-                  col_names = c("Timestamp", "Record", "Sh1", "Sh2", "Sh3", "Sh4", "Sh5", "Sh6"), skip = 4) 
+shore <- read_csv("../SERC/sapflow_data/raw_data/CR1000-LINE_Table1_20190829.dat", 
+                  col_names = c("Timestamp", "Record", "L1", "L2", "L3", "L4", "L5", "L6"), skip = 4) 
   shore$Timestamp <- ymd_hms(shore$Timestamp)
 
 # Met data
-load("../SERC/SERC_met_data_for_Charlotte_&_Stephanie.Rdata")
+list.files("../SERC/met_data/PAR/", full.names = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows() %>% 
+  mutate(Timestamp = ymd_hms(endDateTime), PAR = PARMean) %>% 
+  select(Timestamp, PAR) -> PAR
+
+list.files("../SERC/met_data/TRH/", full.names = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows() %>% 
+  mutate(Timestamp = ymd_hms(endDateTime), RH = RHMean, Temp = tempRHMean) %>% 
+  select(Timestamp, RH, Temp) -> TRH
+
+left_join(PAR, TRH) %>% 
+  mutate(es = (0.6108 *exp((17.27 * Temp)/(273.3 + Temp))), VPD_kPa = ((100 - RH) * es)/100) -> wx_data
 
 # Create 30 minute averages
-cat("Creating 30 minute averages...")
-averaged <- SERC_met %>% 
-  group_by(Date = cut(date.time, breaks = "30 min"), variable) %>% 
-  summarize(mean = mean(value, na.rm = TRUE), date.time = mean(date.time), n = n()) 
-averaged$Timestamp <- ymd_hms(averaged$Date)
-
-averaged <- averaged %>%
-  separate(Date, into = c("Date", "time"), sep = " ") %>%
-  separate(time, into = c("h", "m"), sep = ":")
-averaged$time <- paste0(hour(averaged$date.time), averaged$m)
-
-# Pull out data needed for Baseliner
-cat("Gathering necessary weather data...")
-met_wx <- tibble(Timestamp = averaged$Timestamp[which(averaged$variable == "PAR")],
-                 date = averaged$Date[which(averaged$variable == "PAR")],
-                 time = averaged$time[which(averaged$variable == "PAR")],
-                 PAR = averaged$mean[which(averaged$variable == "PAR")] * 0.327,
-                 es = (6.11 *10^((7.5 * averaged$mean[which(averaged$variable == "Temp")])/(273.3 +  averaged$mean[which(averaged$variable == "Temp")])))/10,
-                 VPD = ((100 - averaged$mean[which(averaged$variable == "RH")]) * es)/100)
+# cat("Creating 30 minute averages...")
+# averaged <- SERC_met %>% 
+#   group_by(Date = cut(date.time, breaks = "30 min"), variable) %>% 
+#   summarize(mean = mean(value, na.rm = TRUE), date.time = mean(date.time), n = n()) 
+# averaged$Timestamp <- ymd_hms(averaged$Date)
+# 
+# averaged <- averaged %>%
+#   separate(Date, into = c("Date", "time"), sep = " ") %>%
+#   separate(time, into = c("h", "m"), sep = ":")
+# averaged$time <- paste0(hour(averaged$date.time), averaged$m)
+# 
+# # Pull out data needed for Baseliner
+# cat("Gathering necessary weather data...")
+# met_wx <- tibble(Timestamp = averaged$Timestamp[which(averaged$variable == "PAR")],
+#                  date = averaged$Date[which(averaged$variable == "PAR")],
+#                  time = averaged$time[which(averaged$variable == "PAR")],
+#                  PAR = averaged$mean[which(averaged$variable == "PAR")] * 0.327,
+#                  es = (6.11 *10^((7.5 * averaged$mean[which(averaged$variable == "Temp")])/(273.3 +  averaged$mean[which(averaged$variable == "Temp")])))/10,
+#                  VPD = ((100 - averaged$mean[which(averaged$variable == "RH")]) * es)/100)
 
 # Truncate based on available weather data
 cat("Combining into single dataframe...")
