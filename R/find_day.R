5f75a71d97ec1282a2c579084027ef78732110ed
Stephanie Pennington <spenn1@terpmail.umd.edu>
2020-01-23 11:27:51 -0500

    Framework for matching function

---
+++ R/find_day.R
@@ -0,0 +1,8 @@
+## Function to match days with similar weather conditions
+## Created 1/20/2020 for Rs-Js analysis | Stephanie Pennington
+
+find_day <- function(cloudy_day, sunny_day, limits) {
+  
+  
+  
+}
