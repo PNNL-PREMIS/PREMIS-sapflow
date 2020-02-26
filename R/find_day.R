7e0dc31abaacb00184853c00dede6fcec119425c
Stephanie Pennington <spenn1@terpmail.umd.edu>
2020-02-26 17:28:18 -0500

    Update find_day.R

--- R/find_day.R
+++ R/find_day.R
@@ -1,33 +1,55 @@
 ## Function to match days with similar weather conditions
 ## Created 1/20/2020 for Rs-Js analysis | Stephanie Pennington
 
-matching_weather_days <- function(doy, weather_data, tair_delta, day_delta) {
+similar_days <- function(doy, climate_data, constraints) {
   # Find all days in a window (of size `window`) around `doy` for
   # which the air temperature is within `tair_delta` of the tair on `doy`
   
   # doy: Day of year, an integer
-  # weather_data: A data frame with at least `doy` and `tair` (degC) columns
-  # tair_delta: A double giving maximum tair difference
-  # day_delta: A integer giving maximum doy difference
+  # climate_data: A data frame with at least `doy` and `tair` (degC) columns
+  # constraints: A vector with at least one climate variable giving maximum difference
   
   # Returns a vector of doys, integers matching these conditions
   
   # Note this function will need to handle edge cases, when doy is close
   # to the beginning or end of the time series
-  tair <- weather_data$Tair[which(weather_data$DOY == doy)] 
   
-  filter(weather_data, Tair <= tair[i] + tair_delta, Tair >= tair[i] - tair_delta, 
-         DOY <= doy + day_delta[i], DOY >= doy[i] - day_delta) -> x
+  #needs daily values of climate data
   
-  subset(x, DOY != doy)
+  if(any(duplicated(climate_data$DOY))) {
+    stop()
+  }
+  
+  filter(climate_data, DOY == doy) %>% 
+    select(names(constraints)) -> test
+  
+  filter(climate_data, Tair <= test$Tair + constraints[["Tair"]], Tair >= test$Tair - constraints[["Tair"]], 
+         DOY <= test$DOY + constraints[["DOY"]], DOY >= test$DOY - constraints[["DOY"]]) -> x
+  
+  filter(x, DOY != test$DOY)
   
 }
 
 
-wd <- data.frame(DOY = 1:6, Tair = c(2, 2.1, 2.2, -1, 2.1, 1.5))
+# Plan
+# 1. similar_days() with test data on GitHub
+# 2. compute mean PAR by day, select sunny days
+# 3. find similar days for each sunny day
+# 4. select cloudy days from those similar days
+# 5. now we have sunny-cloudy pairs (maybe >1 cloudy per sunny) to analyze
 
-matching_weather_days(1, wd, tair_delta = 1, day_delta = 1)   # returns 2
+wd <- data.frame(DOY = c(1,2,3,4,4,5), Tair = c(2, 2.1, 2.2, -1, 2.1, 1.5))
+constraints <- c(4, 7)
+names(constraints) <- c("Tair", "DOY")
+
+similar_days(1, wd, constraints)   # returns 2
+
 matching_weather_days(1, wd, tair_delta = 0.01, day_delta = 1)   # returns c()
 matching_weather_days(3, wd, tair_delta = 1, day_delta = 2)   # returns c(1, 2, 5)
+
+#let's try an example with multiple DOY matches
+constraints <- c(0.01, 2)
+names(constraints) <- c("Tair", "DOY")
+similar_days(4, wd, constraints_2)
 
 
